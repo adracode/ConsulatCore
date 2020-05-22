1f61becd09ad1d4871dd6f31f929b2a9c6813704
Elfas <amizosgame@gmail.com>
2020-05-22 11:54:11 +0200

    Fix vanish
    Un modérateur déconnectant en staff mode restait invisible à la reconnexion

--- src/main/java/fr/amisoz/consulatcore/players/SPlayerManager.java
+++ src/main/java/fr/amisoz/consulatcore/players/SPlayerManager.java
@@ -128,6 +128,9 @@ public class SPlayerManager implements Listener {
                     bukkitPlayer.removePotionEffect(effect.getType());
                 }
             }
+
+            Bukkit.getOnlinePlayers().forEach(onlinePlayer -> onlinePlayer.showPlayer(ConsulatCore.getInstance(), player.getPlayer()));
+
             bukkitPlayer.getInventory().setContents(player.getStockedInventory());
         }
         
