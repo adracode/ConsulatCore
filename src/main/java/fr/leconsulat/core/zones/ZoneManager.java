1b6387fbee261f1a1a97a4eea661bb250be0f587
Antoine <48187865+adracode@users.noreply.github.com>
2020-09-21 09:05:13 +0200

    HOTFIX

--- src/main/java/fr/leconsulat/core/zones/ZoneManager.java
+++ src/main/java/fr/leconsulat/core/zones/ZoneManager.java
@@ -278,23 +278,24 @@ public class ZoneManager {
                     UUID.fromString(result.getString("owner")),
                     result.getDouble("money")
             );
+            File file = FileUtils.loadFile(ConsulatAPI.getConsulatAPI().getDataFolder(), "cities/" + city.getUniqueId() + ".dat");
+            if(!file.exists()){
+                ConsulatAPI.getConsulatAPI().log(Level.WARNING, "City " + city.getName() + ", owner " + city.getOwner() + ", money " + city.getMoney() + " doesn't have file, deleting...");
+                //Refund player
+                SPlayerManager.getInstance().addMoney(city.getOwner(), City.CREATE_TAX);
+                SPlayerManager.getInstance().addMoney(city.getOwner(), city.getMoney());
+                deleteCity(city);
+            }
             try {
-                File file = FileUtils.loadFile(ConsulatAPI.getConsulatAPI().getDataFolder(), "cities/" + city.getUniqueId() + ".dat");
-                if(!file.exists()){
-                    ConsulatAPI.getConsulatAPI().log(Level.WARNING, "City " + city.getName() + ", owner " + city.getOwner() + ", money " + city.getMoney() + " doesn't have file, deleting...");
-                    //Refund player
-                    SPlayerManager.getInstance().addMoney(city.getOwner(), City.CREATE_TAX);
-                    SPlayerManager.getInstance().addMoney(city.getOwner(), city.getMoney());
-                    deleteCity(city);
-                }
                 NBTInputStream is = new NBTInputStream(new FileInputStream(file));
                 CompoundTag cityTag = is.read();
                 is.close();
                 city.loadNBT(cityTag);
-                addCity(city);
-            } catch(IOException e){
+            } catch(Exception e){
                 e.printStackTrace();
+                continue;
             }
+            addCity(city);
         }
     }
     
