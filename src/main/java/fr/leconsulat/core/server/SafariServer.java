e7319d2d8377be7648e6d33c1bc4cb23ae478ac7
Antoine <48187865+adracode@users.noreply.github.com>
2020-10-01 19:08:44 +0200

    UPD - Synchronisation de la vie et nourriture

--- src/main/java/fr/leconsulat/core/server/SafariServer.java
+++ src/main/java/fr/leconsulat/core/server/SafariServer.java
@@ -8,6 +8,7 @@ import fr.leconsulat.api.player.stream.PlayerOutputStream;
 import fr.leconsulat.api.redis.RedisManager;
 import fr.leconsulat.api.server.Server;
 import fr.leconsulat.core.ConsulatCore;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.redisson.api.RTopic;
 
@@ -22,12 +23,15 @@ public class SafariServer extends Server {
     public SafariServer(){
         super(ConsulatAPI.getConsulatAPI().isDevelopment() ? "testsafari" : "safari", true);
         RedisManager.getInstance().register(ConsulatAPI.getConsulatAPI().isDevelopment() ? "LoadPlayerDataTestsurvie" : "LoadPlayerDataSurvie",
-                String.class, (channel, data) -> CPlayerManager.getInstance().loadPlayerData(Base64.getDecoder().decode(data)));
+                String.class, (channel, data) -> {
+                    Bukkit.getScheduler().runTask(ConsulatCore.getInstance(), () ->
+                            CPlayerManager.getInstance().loadPlayerData(Base64.getDecoder().decode(data)));
+                });
         RedisManager.getInstance().register(ConsulatAPI.getConsulatAPI().isDevelopment() ? "SavePlayerDataTestsurvie" : "SavePlayerDataSurvie",
                 String.class, (channel, data) -> CPlayerManager.getInstance().savePlayerData(Base64.getDecoder().decode(data)));
         RedisManager.getInstance().register("AskPlayerData" + (ConsulatAPI.getConsulatAPI().isDevelopment() ? "Testsurvie" : "Survie"),
                 String.class, (channel, uuid) -> loadOnSafari.publishAsync(Base64.getEncoder().encodeToString(
-                        new OfflinePlayerOutputStream(UUID.fromString(uuid)).writeLevel().writeInventory().writeActiveEffects().send())));
+                        new OfflinePlayerOutputStream(UUID.fromString(uuid)).writeFully().send())));
     }
     
     @Override
@@ -39,7 +43,7 @@ public class SafariServer extends Server {
     public void onPlayerConnect(ConsulatPlayer player){
         player.setDisconnectHandled(true);
         player.setInventoryBlocked(true);
-        loadOnSafari.publishAsync(Base64.getEncoder().encodeToString(new PlayerOutputStream(player.getPlayer()).writeLevel().writeInventory().writeActiveEffects().send()));
+        loadOnSafari.publishAsync(Base64.getEncoder().encodeToString(new PlayerOutputStream(player.getPlayer()).writeFully().send()));
     }
     
     @Override
